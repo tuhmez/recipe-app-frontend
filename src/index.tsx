c643ab338850f7076f4d589c06b30ed7e537af30
Chris Tamez <ctamez232@gmail.com>
2021-03-19 15:23:15 -0400

    added storybook

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { connectClient } from './communications';
+
+connectClient();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
