7a7b8b425c01e7a4dbe9ad039f118d548bec8434
Chris Tamez <ctamez232@gmail.com>
2021-05-25 08:49:18 -0500

    added search app bar

--- src/components/SearchAppBar/SearchAppBar.tsx
+++ src/components/SearchAppBar/SearchAppBar.tsx
@@ -1,46 +1,56 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import MenuIcon from '@material-ui/icons/Menu';
+import React, { useState } from 'react';
+import {
+  AppBar,
+  InputBase,
+  Theme,
+  Toolbar,
+  Typography
+} from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-
-import { theme } from '../../app-styles';
 
 import { useStyles } from './styles';
 
+interface SearchAppBarProps {
+  searchFunction: (searchTerm: string) => void;
+  searchHeader: string;
+  theme: Theme;
+}
 
-const SearchAppBar = () => {
+const SearchAppBar = (props: SearchAppBarProps) => {
+  const { searchFunction, searchHeader, theme } = props;
+
   const classes = useStyles(theme);
+  const [ search, setSearch ] = useState('');
+
+  const changeSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+  const searchKeyPress = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      searchFunction(search);
+    }
+  };
 
   return (
     <div className={classes.root}>
-      <AppBar color='primary' position="static">
+      <AppBar color='primary' position='static'>
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography className={classes.title} variant="h6" noWrap>
-            Recipes
+          <Typography className={classes.title} variant='h6' noWrap>
+            {searchHeader}
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
               <SearchIcon />
             </div>
             <InputBase
-              placeholder="Search…"
+              placeholder='Search…'
               classes={{
                 root: classes.inputRoot,
                 input: classes.inputInput,
               }}
               inputProps={{ 'aria-label': 'search' }}
+              onChange={changeSearchInput}
+              onKeyDown={searchKeyPress}
             />
           </div>
         </Toolbar>
