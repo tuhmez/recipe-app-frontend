2d70368b4dd64c532fc2aa11573816875d608ec1
Chris Tamez <ctamez232@gmail.com>
2021-10-13 15:41:46 -0500

    fixed recipe form import

--- src/stories/RecipeForm.stories.tsx
+++ src/stories/RecipeForm.stories.tsx
@@ -1,11 +1,7 @@
-import { action } from '@storybook/addon-actions';
-
 import { theme } from '../app-styles';
 import RecipeForm from '../components/RecipeForm';
 import { ThemeProvider } from '@material-ui/core';
-
-import { recipeExample, recipeExampleNoImages } from '../utilities';
-import { RecipeMessage } from '../proto/recipe_pb';
+import { exampleRecipe } from '../common/data';
 
 const storyExport = {
   title: 'RecipeForm',
@@ -17,7 +13,7 @@ export default storyExport;
 export const baseRecipeForm = () => {
   return (
     <ThemeProvider theme={theme}>
-      <RecipeForm data={new RecipeMessage().toObject()}/>
+      <RecipeForm data={exampleRecipe}/>
     </ThemeProvider>
   )
 };
