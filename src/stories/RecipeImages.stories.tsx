e1a7a328d4db8ebf582f599a958c7c1b1861b2ed
Chris Tamez <ctamez232@gmail.com>
2021-10-27 17:58:14 -0500

    placeholder image component story

---
+++ src/stories/RecipeImages.stories.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider } from '@material-ui/core';
+
+import { theme } from '../app-styles';
+import { RecipeImages } from '../components/RecipeImages';
+import { exampleRecipe } from '../common/data';
+
+const storyExport = {
+  title: 'RecipeImages',
+  component: RecipeImages
+};
+
+export default storyExport;
+
+export const BaseRecipeImages = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <RecipeImages data={exampleRecipe.images}/>
+    </ThemeProvider>
+  )
+};
