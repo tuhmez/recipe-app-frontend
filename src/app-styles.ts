5dc2819a4699a9c22337e2638ec3203cfaec8cfe
Chris Tamez <ctamez232@gmail.com>
2021-11-08 21:18:08 -0600

    fixing error

--- src/app-styles.ts
+++ src/app-styles.ts
@@ -1,6 +1,6 @@
-import { createMuiTheme } from '@material-ui/core';
+import { createTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors'
-export const theme = createMuiTheme({
+export const theme = createTheme({
   palette: {
     primary: {
       main: '#039be5',
