a83b60e16eeb7ef67d69e1973e622240bf939700
Chris Tamez <ctamez232@gmail.com>
2021-11-18 17:17:02 -0600

    create recipe page should be ready to go

---
+++ src/containers/CreateRecipePage/index.tsx
@@ -0,0 +1,31 @@
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+
+import { emptyRecipe } from '../../common/data';
+import { IRecipe } from '../../common/types';
+import { RecipeForm } from '../../components/RecipeForm';
+import { ADD_RECIPE_REQUEST } from '../../redux/reducer';
+import { SocketClient } from '../../socket';
+
+export interface Props {
+  socket: SocketClient;
+}
+
+export const CreateRecipePage = (props: Props) => {
+  // Props deconstruction
+  const { socket } = props;
+  // Dispatch
+  const dispatch = useDispatch();
+  // Navigate
+  const navigate = useNavigate();
+  // Handlers
+  const onLeaveFormHandler = () => {
+    navigate(-1);
+  };
+  const onSubmitFormHandler = (recipe: IRecipe) => {
+    dispatch({ type: ADD_RECIPE_REQUEST });
+    socket.addRecipe(recipe);
+  };
+
+  return <RecipeForm data={emptyRecipe} onCancelFormAction={onLeaveFormHandler} onSubmitFormAction={onSubmitFormHandler}/>
+};
