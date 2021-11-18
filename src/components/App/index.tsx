17e998ebeedf24c7358fd1c2c15741a6b22e2b58
Chris Tamez <ctamez232@gmail.com>
2021-11-18 17:07:27 -0600

    fixed app bar styling

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Fab, ThemeProvider } from '@material-ui/core';
+import { Container, CssBaseline, Fab } from '@material-ui/core';
 import { Add } from '@material-ui/icons';
+import { useDispatch } from 'react-redux';
 
 import { SocketClient } from '../../socket';
 import { theme } from '../../app-styles';
 import { IRecipe } from '../../common/types';
-import { IErrorResponse } from '../../socket/interfaces';
-import './App.css';
+import {
+  IAddRecipeResponse,
+  IEditRecipeResponse,
+  IDeleteRecipeResponse,
+  IGetRecipesResponse,
+} from '../../socket/interfaces';
+import { Routes } from '../../routes/routes';
 import SearchAppBar from '../SearchAppBar';
 import { useStyles } from './styles';
+import './App.css';
 
 export const App = () => {
   // App constants
@@ -16,15 +23,21 @@ export const App = () => {
   const serverPort = process.env.SERVER_PORT || '3001';
   // States
   const [ socket, setSocket ] = useState<SocketClient | undefined>(undefined);
+  // Dispatch
+  const dispatch = useDispatch();
   // Effects
   useEffect(() => {
     const createdSocket = new SocketClient(serverAddress, serverPort, {
-      addRecipeResponseFn: (res: IRecipe) => console.log(res),
-      editRecipeResponseFn: (res: IRecipe) => console.log(res),
-      deleteRecipeResponseFn: (res: IRecipe) => console.log(res),
-      getRecipesResponseFn: (res: IRecipe[]) => console.log(res),
-      getRecipeByIdResponseFn: (res: IRecipe) => console.log(res),
-      errorResponseFn: (err: IErrorResponse) => console.log(err)
+      addRecipeResponseFn: (res: IAddRecipeResponse) => {
+        if (res.error) {
+          // dispatch failure
+        } else {
+          // dispatch success
+        }
+      },
+      editRecipeResponseFn: (res: IEditRecipeResponse) => console.log(res),
+      deleteRecipeResponseFn: (res: IDeleteRecipeResponse) => console.log(res),
+      getRecipesResponseFn: (res: IGetRecipesResponse) => console.log(res)
     });
     setSocket(createdSocket);
     return () => {
@@ -41,20 +54,16 @@ export const App = () => {
   if (!socket) return <div>we loading...</div>;
 
   return (
-    <ThemeProvider theme={theme}>
+    <div>
+      <CssBaseline />
       <SearchAppBar
         searchFunction={(searchTerm: string) => console.log(searchTerm)}
         searchHeader='Recipes'
         theme={theme}
       />
-      <Fab
-        color='primary'
-        aria-label='add'
-        size='large'
-        className={classes.fab}
-      >
-        <Add />
-      </Fab>
-    </ThemeProvider>
+      <Container className={classes.container}>
+        <Routes socket={socket}/>
+      </Container>
+    </div>
   );
 };
--- src/components/App/styles.ts
+++ src/components/App/styles.ts
@@ -1,6 +1,11 @@
 import { makeStyles } from '@material-ui/core';
 
 export const useStyles = makeStyles(theme => ({
+  // 64 px is the app bar height
+  container: {
+    maxHeight: 'calc(100vh - 64px)',
+    paddingTop: `calc(64px + ${theme.spacing(2)}px)`
+  },
   fab: {
     position: 'fixed',
     top: 'auto',
