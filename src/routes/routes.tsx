57f42535b9f727d13d75ced00d5f93f7e66b68fe
Chris Tamez <ctamez232@gmail.com>
2021-12-02 17:44:05 -0600

    adding available routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,7 +3,9 @@ import { Routes as AppRoutes, Route} from 'react-router-dom';
 import { possibleRoutes } from './routeConstants';
 import { Main } from '../containers/Main';
 import { CreateRecipePage } from '../containers/CreateRecipePage';
+import { ViewRecipePage } from '../containers/ViewRecipePage';
 import { SocketClient } from '../socket';
+import { EditRecipePage } from '../containers/EditRecipePage';
 
 export interface Props {
   socket: SocketClient;
@@ -13,12 +15,14 @@ export const Routes = (props: Props) => {
   // Props desconstruction
   const { socket } = props;
   // Constants
-  const { main, createRecipePage } = possibleRoutes;
+  const { main, createRecipePage, viewRecipePage, editRecipePage } = possibleRoutes;
 
   return (
     <AppRoutes>
       <Route path={main} element={<Main />} />
       <Route path={createRecipePage} element={<CreateRecipePage socket={socket} />}/>
+      <Route path={viewRecipePage} element={<ViewRecipePage socket={socket}/>}/>
+      <Route path={editRecipePage} element={<EditRecipePage socket={socket}/>}/>
     </AppRoutes>
   )
 }
