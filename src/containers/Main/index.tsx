1c47a9921411e6eae90a88eb9cfe3b4f52b39df1
Chris Tamez <ctamez232@gmail.com>
2021-12-04 11:41:46 -0600

    added fab, main page

---
+++ src/containers/Main/index.tsx
@@ -0,0 +1,47 @@
+import { Fab, useTheme } from '@material-ui/core';
+import { Add } from '@material-ui/icons';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import { selectRecipes } from '../../redux/selectors';
+import { possibleRoutes, viewRecipePageNavigator } from '../../routes/routeConstants';
+import { RecipeCardGrid } from '../../components/RecipeCardGrid';
+import { useStyles } from './styles';
+import { IRecipe } from '../../common/types';
+
+export const Main = () => {
+  // Dispatch
+  const dispatch = useDispatch();
+  // Selectors
+  const recipes = useSelector(selectRecipes);
+  // Navigation
+  const navigate = useNavigate();
+  // Handlers
+  const onClickAddRecipe = () => {
+    navigate(createRecipePage);
+  };
+  const onCardClick = (recipe: IRecipe) => {
+    navigate(viewRecipePageNavigator(recipe.name));
+  };
+  // Styles
+  const classes = useStyles();
+  const theme = useTheme();
+  // Constants
+  const { createRecipePage } = possibleRoutes;
+
+  return (
+    <div>
+      <RecipeCardGrid recipes={recipes} theme={theme} handleCardClick={onCardClick} />
+      <Fab
+        color='primary'
+        aria-label='add'
+        size='large'
+        className={classes.fab}
+        onClick={onClickAddRecipe}
+      >
+        <Add />
+      </Fab>
+    </div>
+  )
+};
---
+++ src/containers/Main/styles.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles(theme => ({
+  fab: {
+    position: 'fixed',
+    top: 'auto',
+    bottom: 20,
+    right: 20,
+    left: 'auto',
+    margin: 0
+  }
+}));
