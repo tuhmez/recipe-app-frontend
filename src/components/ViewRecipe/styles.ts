c9721f9eb6faef200ba3238f83cf1bfbe4c3b5c0
Chris Tamez <ctamez232@gmail.com>
2022-04-14 09:19:14 -0500

    fixing centering issue on desktop

--- src/components/ViewRecipe/styles.ts
+++ src/components/ViewRecipe/styles.ts
@@ -21,6 +21,7 @@ export const useStyles = makeStyles(theme => ({
   img: {
     height: '50vh',
     width: '80vw',
+    maxWidth: '1248px',
     objectFit: 'contain',
     overflow: 'hidden',
     display: 'block',
