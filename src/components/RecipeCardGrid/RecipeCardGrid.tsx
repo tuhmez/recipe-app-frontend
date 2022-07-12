6561235cf71349d263155f2a79f797a7c9588347
Chris Tamez <ctamez232@gmail.com>
2022-07-11 23:29:43 -0500

    filtering and sorting done, need to move default states to redux

--- src/components/RecipeCardGrid/RecipeCardGrid.tsx
+++ src/components/RecipeCardGrid/RecipeCardGrid.tsx
@@ -1,5 +1,5 @@
 import { Button, Grid, Theme, Typography } from '@material-ui/core';
-import { useEffect } from 'react';
+import { useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { IRecipe } from '../../common/types';
@@ -9,15 +9,30 @@ import { selectSearchTerm } from '../../redux/selectors';
 import { RecipeCard } from '../RecipeCard';
 import { useStyles } from './styles';
 
+export interface ISort {
+  favorites?: boolean;
+  type?: boolean;
+  difficulty?: boolean;
+  alphabetical?: boolean;
+}
+
+export type IFilter = {
+  [key in 'Favorites' | 'Difficulty' | 'Type']: {
+    [key: string]: boolean;
+  };
+};
+
 export interface Props {
   recipes: IRecipe[];
   theme: Theme;
   handleCardClick: (recipe: IRecipe) => void;
+  sort?: ISort;
+  filter?: IFilter;
 }
 
 export const RecipeCardGrid = (props: Props) => {
   // Props deconstruction
-  const { handleCardClick, recipes, theme } = props;
+  const { handleCardClick, filter, recipes, sort, theme } = props;
   // Selectors
   const searchTerm = useSelector(selectSearchTerm);
   // Dispatch
@@ -25,36 +40,126 @@ export const RecipeCardGrid = (props: Props) => {
   // Styles
   const classes = useStyles();
 
-  const recipeCards = recipes.map(recipe => {
-    const onHandleCardClick = () => {
-      handleCardClick(recipe);
+  const recipeCards = useMemo(() => {
+    return recipes.filter(recipe => {
+      if (filter) {
+        const filterArray = [ filter.Difficulty, filter.Favorites, filter.Type ];
+        const canSkipFilter = filterArray.map((v) => Object.values(v)).flat().every((v) => !v);
+        
+        if (canSkipFilter) return recipe;
+
+        const selectedFilters = new Map();
+        selectedFilters.set("difficulty", []);
+        selectedFilters.set("favorited", []);
+        selectedFilters.set("type", []);
+
+        filterArray.forEach((f, i) => {
+          Object.entries(f).forEach(([k, v]) => {
+            let arrayToGet = "";
+            if (v === true) {
+              if (i === 0) {
+                arrayToGet = "difficulty";
+              } else if (i === 1) {
+                arrayToGet = "favorited";
+              } else {
+                arrayToGet = "type";
+              }
+
+              const filteredArray = selectedFilters.get(arrayToGet);
+              filteredArray.push(k.toLowerCase());
+              selectedFilters.set(arrayToGet, filteredArray);
+            }
+          });
+          
+        });
+
+        const recipeMatch: boolean[] = [];
+
+        selectedFilters.forEach((v, k) => {
+          if (v.length === 0) return;
+          if (k === "favorited") {
+            if (v.length === 2) {
+              recipeMatch.push(true);
+            } else {
+              recipeMatch.push(recipe.favorited === (v[0] === "favorited"));
+            }
+          } else {
+            // @ts-ignore
+            recipeMatch.push(v.includes(recipe[k].toLowerCase()));
+          }
+        });
+
+        return recipeMatch.every((v) => v === true);
+      }
+
+      return false;
+    })
+    .sort((a, b) => {
+      if (sort) {
+        if (sort.alphabetical && sort.alphabetical === true) {
+          if (a.name > b.name) return 1;
+          if (a.name < b.name) return -1;
+          
+          return 0;
+        } else if (sort.difficulty && sort.difficulty === true) {
+
+        } else if (sort.favorites && sort.favorites === true) {
+          if (a.favorited && !b.favorited) return -1;
+          if (!a.favorited && b.favorited) return 1;
+
+          return 0;
+        } else if (sort.type && sort.type === true) {
+          if (a.type > b.type) return 1;
+          if (a.type < b.type) return -1;
+
+          return 0;
+        } else {
+          return 0;
+        }
+      }
+
+      return 0;
+    })
+    .map(recipe => {
+      const onHandleCardClick = () => {
+        handleCardClick(recipe);
+      };
+      return (
+        <Grid item key={`${recipe.name}-${recipe.recipeId}`}>
+          <RecipeCard
+            recipe={recipe}
+            theme={theme}
+            onCardClick={onHandleCardClick}
+            key={`${recipe.name}-${recipe.recipeId}`}
+          />
+        </Grid>
+      );
+    });
+  }, [filter, handleCardClick, recipes, sort, theme]);
+
+    // make width and wrap elements
+    // |---- item --- item ----|
+    // |-----------------------|
+    // |---- item --- item ----|
+
+  const subGrid = useMemo(() => {
+    const grid: JSX.Element[] = [];
+    for (let i = 0; i < recipeCards.length; i += 2) {
+      let subGridItems: any = [];
+      const firstItem = recipeCards[i];
+      const secondItem = recipeCards[i + 1];
+      if (firstItem) subGridItems.push(firstItem);
+      if (secondItem) subGridItems.push(secondItem);
+  
+      grid.push(
+        <Grid container item justifyContent='center' spacing={1} key={`subgrid-${grid.length}`}>
+          {subGridItems}
+        </Grid>
+      );
+
     };
-    return (
-      <Grid item key={`${recipe.name}-${recipe.recipeId}`}>
-        <RecipeCard
-          recipe={recipe}
-          theme={theme}
-          onCardClick={onHandleCardClick}
-          key={`${recipe.name}-${recipe.recipeId}`}
-        />
-      </Grid>
-    );
-  });
-
-  let subGrid: JSX.Element[] = [];
-  for (let i = 0; i < recipeCards.length; i += 2) {
-    let subGridItems: any = [];
-    const firstItem = recipeCards[i];
-    const secondItem = recipeCards[i + 1];
-    if (firstItem) subGridItems.push(firstItem);
-    if (secondItem) subGridItems.push(secondItem);
-
-    subGrid.push(
-      <Grid container item justifyContent='center' spacing={1} key={`subgrid-${subGrid.length}`}>
-        {subGridItems}
-      </Grid>
-    )
-  };
+    return grid;
+  }, [recipeCards])
   
   const onClearSearch = () => {
     dispatch({ type: CLEAR_SEARCH });
