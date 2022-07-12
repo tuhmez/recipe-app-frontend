3be1242e13c63b145c4f0c5e30ee0765a6b5c175
Chris Tamez <ctamez232@gmail.com>
2022-07-12 17:52:34 -0500

    changed favorited indicator

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardMedia, CardActionArea, Theme, Typography } from 
 import { IRecipe } from '../../common/types';
 import NoImagePlaceholder from '../../resources/no_image_placeholder.png';
 import { useStyles } from './styles';
+import { Favorite } from '@material-ui/icons';
 
 export interface Props {
   onCardClick: () => void;
@@ -25,16 +26,22 @@ export const RecipeCard = (props: Props) => {
   const mainPicture = (recipe.images.length > 0 ? recipe.images[0] : NoImagePlaceholder) as string;
 
   return (
-    <Card variant='outlined' className={recipe.favorited ? classes.favoritedCard : classes.card}>
-      <CardActionArea
-        onClick={handleCardClick}
-      >
-        <CardMedia
-          className={classes.media}
-          image={mainPicture}
-          title={recipe.name}
-          component='img'
-        />
+    <Card variant='outlined' className={classes.card}>
+      <CardActionArea onClick={handleCardClick}>
+        <div style={{ position: 'relative'}}>
+          <CardMedia
+            className={classes.media}
+            image={mainPicture}
+            title={recipe.name}
+            component='img'
+          />
+          {recipe.favorited && (
+            <>
+              <span id="favorite-span" className={classes.favoriteBanner}></span>
+              <Favorite fontSize='small' className={classes.favoriteIcon} />
+            </>
+          )}
+        </div>
         <CardContent>
           <Typography variant='h6' component='h2'>
             {recipe.name}
--- src/components/RecipeCard/styles.ts
+++ src/components/RecipeCard/styles.ts
@@ -10,9 +10,25 @@ export const useStyles = makeStyles((theme) => ({
     borderColor: red[500]
   },
   media: {
+    display: 'block',
     height: 100,
   },
   favoriteIcon: {
+    position: 'absolute',
+    top: '3px',
+    right: '3px',
     color: red[500]
+  },
+  favoriteBanner: {
+    position: 'absolute',
+    top: '-5px',
+    right: '-20px',
+    width: 0,
+    height: 0,
+    borderLeft: '30px solid black',
+    borderRight: '30px solid black',
+    
+    borderBottom: `30px solid black`,
+    transform: 'rotate(45deg)'
   }
 }));
