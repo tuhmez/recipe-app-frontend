08dfcfc39553544f29f8b13a16c294f9b2a58301
Chris Tamez <ctamez232@gmail.com>
2024-04-18 09:46:48 -0500

    add provider, impl new snackbar function for better usability w storybook

--- src/stories/RecipeForm.stories.tsx
+++ src/stories/RecipeForm.stories.tsx
@@ -3,26 +3,37 @@ import { RecipeForm } from '../components/RecipeForm';
 import { ThemeProvider } from '@material-ui/core';
 import { emptyRecipe, exampleRecipe } from '../common/data';
 import { action } from '@storybook/addon-actions';
+import { Provider } from 'react-redux';
+
+import { combineReducers, createStore } from 'redux';
+
+const store = createStore(combineReducers((state: any, action: any) => {}));
 
 const storyExport = {
   title: 'RecipeForm',
   component: RecipeForm
 };
 
+
 export default storyExport;
 
 export const BaseRecipeForm = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <RecipeForm data={emptyRecipe} onCancelFormAction={action('onLeaveForm')} onSubmitFormAction={action('onSubmitForm')}/>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <RecipeForm data={emptyRecipe} onCancelFormAction={action('onLeaveForm')} onSubmitFormAction={action('onSubmitForm')} enqueueSnackbar={action('enqueueSnackbar')}/>
+      </ThemeProvider>
+
+    </Provider>
   );
 };
 
 export const BaseRecipeFormEdit = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <RecipeForm data={exampleRecipe} isEdit onCancelFormAction={action('onLeaveForm')} onSubmitFormAction={action('onSubmitForm')}/>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <RecipeForm data={exampleRecipe} isEdit onCancelFormAction={action('onLeaveForm')} onSubmitFormAction={action('onSubmitForm')} enqueueSnackbar={action('enqueueSnackbar')}/>
+      </ThemeProvider>
+    </Provider>
   );
 };
